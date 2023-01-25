48ebad058b3cbca1e44a6e7eb09dd469ca866dd3
Ajay Yadav <yajay04112000@gmail.com>
2023-01-25 15:46:08 +0530

    minor bugs

--- routes/post.js
+++ routes/post.js
@@ -2,10 +2,13 @@ import express from 'express';
 import { createPost, FetchAllPosts, FetchPost, DeletePost, LikePost } from '../controllers/post.js';
 import UPLOAD from "../middlewares/post.js";
 import auth from "../middlewares/auth.js";
+import path from "path"
 
 const router = express.Router();
 
-router.post("/createPost", UPLOAD.single("media"), createPost);
+
+
+router.post("/createPost", UPLOAD.single(path.join(__dirname, `../media`)), createPost);
 router.get("/getAllPosts", FetchAllPosts);
 router.get("/getSelectedPost/:id", auth, FetchPost);
 router.delete("/deleteSelectedPost/:id", auth, DeletePost);
